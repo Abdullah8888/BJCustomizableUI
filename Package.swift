7036449c4627865c1ca2729f686638fdb1e293bb
= <abdullahijimoh3.ja@gmail.com>
2024-02-11 18:13:02 +0100

    Resolved bundle id

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "BJCustomizableUI",
+            name: "com.myapp.BJCustomizableUI",
             targets: ["BJCustomizableUI"]),
     ],
     dependencies: [
